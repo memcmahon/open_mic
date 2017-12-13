7d91e4491653b096d92f501b55e5155a41175669
memcmahon <mcmahon.meganelizabeth@gmail.com>
2017-12-13 09:45:29 -0700

    Updates to allow user to perform a routine

--- lib/user.rb
+++ lib/user.rb
@@ -10,4 +10,14 @@ class User
   def learn(joke)
     @jokes << joke
   end
+
+  def tell(audience, joke)
+    audience.jokes << joke
+  end
+
+  def perform_routine_for(audience)
+    @jokes.each do |joke|
+      tell(audience, joke)
+    end
+  end
 end
--- test/user_test.rb
+++ test/user_test.rb
@@ -48,6 +48,39 @@ class UserTest < Minitest::Test
     sal.learn(joke2)
 
     assert_instance_of Joke, sal.jokes[0]
+    assert_equal 1, sal.jokes[0].id
     assert_instance_of Joke, sal.jokes[1]
+    assert_equal 3, sal.jokes[1].id
+  end
+
+  def test_users_can_tell_each_other_jokes
+    sal = User.new("Sal")
+    ali = User.new("Ali")
+
+    joke = Joke.new({id: 13, question: "How does a man on the moon get his hair cut?", anser: "Eclipse it."})
+
+    sal.tell(ali, joke)
+
+    assert_instance_of Joke, ali.jokes[0]
+    assert_equal 13, ali.jokes[0].id
+    assert_equal 1, ali.jokes.count
+  end
+
+  def test_perform_routine_allows_users_to_tell_multiple_jokes
+    joke_1 = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+    joke_2 = Joke.new({id: 2, question: "How do you keep a lion from charging?", answer: "Take away its credit cards."})
+    ilana = User.new("Ilana")
+    josh = User.new("Josh")
+
+    ilana.learn(joke_1)
+    ilana.learn(joke_2)
+
+    ilana.perform_routine_for(josh)
+
+    assert_equal 2, josh.jokes.count
+    assert_instance_of Joke, josh.jokes[0]
+    assert_instance_of Joke, josh.jokes[1]
+    assert_equal 1, josh.jokes[0].id
+    assert_equal 2, josh.jokes[1].id
   end
 end
