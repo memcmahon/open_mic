9a0c529949f4fe0cd427c024c294c25edae70030
memcmahon <mcmahon.meganelizabeth@gmail.com>
2017-12-13 09:09:51 -0700

    Updates with Joke Tests

--- test/joke_test.rb
+++ test/joke_test.rb
@@ -4,5 +4,30 @@ require 'minitest/pride'
 require './lib/joke'
 
 class JokeTest < Minitest::Test
+  def test_it_exists
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+
+    assert_instance_of joke, Joke
+  end
+
+  def test_id_returns_correct_joke_id
+    skip
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+
+    assert_equal 1, joke.id
+  end
+
+  def test_question_returns_correct_joke_question
+    skip
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+
+    assert_equal "Why did the strawberry cross the road?", joke.question
+  end
+
+  def test_answer_returns_correct_joke_answer
+    skip
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+
+    assert_equal "Because his mother was in a jam.", joke.answer
 
 end
