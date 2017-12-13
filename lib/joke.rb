a0ee837efae640b7f36cafd226477bd1ae429464
memcmahon <mcmahon.meganelizabeth@gmail.com>
2017-12-13 09:19:27 -0700

    Updates with Joke Class passing tests

--- lib/joke.rb
+++ lib/joke.rb
@@ -1,3 +1,17 @@
 class Joke
+  def initialize(joke_info)
+    @joke_info = joke_info
+  end
 
+  def id
+    @joke_info[:id]
+  end
+
+  def question
+    @joke_info[:question]
+  end
+
+  def answer
+    @joke_info[:answer]
+  end
 end
